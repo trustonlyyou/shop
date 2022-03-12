29f3de2a1b759d29aef4804ce3e7ef0b491c3d0c
trustonlyyou <trustonlyyou@naver.com>
2022-03-12 18:43:42 +0900

    Item Entity Structure & Item Enity H2 test

---
+++ src/main/java/com/shop/constant/ItemSellStatus.java
@@ -0,0 +1,10 @@
+package com.shop.constant;
+
+/**
+ * 판매중인지, 품절 상태인지
+ * 장점 : compile 시 체크할 수 있다는 장점
+ */
+public enum ItemSellStatus {
+
+    SELL, SOLD_OUT
+}
---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,43 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity // primary key
+@Table(name = "item") // table mapping
+@Getter
+@Setter
+@ToString
+public class Item {
+    @Id
+    @Column(name = "item_id") // column mapping
+    @GeneratedValue(strategy = GenerationType.AUTO) // JPA 구현체가 자동으로 생성 전략 결정
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemNm; // 상품명
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber; // 재고 수량
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail; // 상품 상세 설명
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus; //상품 판매 상태
+
+    private LocalDateTime regTime; // 등록 시간
+
+    private LocalDateTime updateTime; // 수정 시간
+
+
+}
